b5b54ad082d204b4a0a7d9fe24bfb066f06b87a8
En1gma64 <s3823697@student.rmit.edu.au>
2022-04-23 17:03:57 +1000

    Final (Pre presentation)

--- FrontEnd/react-application/src/components/Items/AddItem.js
+++ FrontEnd/react-application/src/components/Items/AddItem.js
@@ -1,8 +1,11 @@
 import React, { Component, useRef } from 'react'
 import jwtDecode from 'jwt-decode';
 import AdminHeader from '../Layout/AdminHeader';
+import AWS from 'aws-sdk';
 
 export default function AddItem() {
+
+    
 
     const[itemName, setItemName] = React.useState('');
     const[desc, setDesc] = React.useState('');
@@ -12,15 +15,66 @@ export default function AddItem() {
     const[size, setSize] = React.useState('');
     const[sex, setSex] = React.useState('');
     const[available] = React.useState('Yes');
-    const[imageLink] = React.useState('https://images.unsplash.com/photo-1578932750294-f5075e85f44a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2xvdGhlcyUyMHNob3B8ZW58MHx8MHx8&w=1000&q=80');
+    const [progress , setProgress] = React.useState(0);
+    var selectedFile = React.useState(null);
+    var imageLink = React.useState('https://cloudcomputing-a1-images.s3.amazonaws.com/House-of-Vintage-best-Toronto-vintage-clothing-stores-1440x1029.jpg');
+
+    const S3_BUCKET ='cloudcomputing-a1-images';
+    const REGION ='us-east-1';
+    const ACCESS_KEY ='ASIAYB2TE3WWXK52KE5E';
+    const SECRET_ACCESS_KEY ='udrtxoXMkMmYoWyOK6oOmNOF/4tasjqsL6hUUV+G';
+    const SESSION_TOKEN = 'FwoGZXIvYXdzEK///////////wEaDAd91AbQ1JJDfBUfcyLNAYwxs6mBAScefmJQwI7XPW3Kj7ogFCjzXLSMYz/TmRVr/x9Tttxwbsi2IN3JKTYS3AaiV8xDp81qz4LoNyZivyMYZgn0nwdailSjZlQiG8nxDxwBF9G9fVnyJ644DtsUwD/cI71I6Q4PBLzeF0KEWKVNJKQnpr1UqkWkzYzZ3fxhl3lgyyEzuPZSU8W27YflK+947N+V6wg3Nn2hQUSeI9X76NaOVtizg8YRp9h237gYQI9e0HMAq+hc5thbxTylsp+IG6InVyXHfDA2DDcomqSOkwYyLWE1g27HnM917wiDfaFQkKoCMGsWVnDXYY6Ta2YKdR/wv8nw4kRG0eSIEeBkqQ==';
+
+    AWS.config.update({
+        accessKeyId: ACCESS_KEY,
+        secretAccessKey: SECRET_ACCESS_KEY,
+        sessionToken: SESSION_TOKEN
+    })
+
+    // const config = {
+    //     bucketName: S3_BUCKET,
+    //     region: REGION,
+    //     accessKeyId: ACCESS_KEY,
+    //     secretAccessKey: SECRET_ACCESS_KEY,
+    //     SessionToken: SESSION_TOKEN
+    // }
+
+    const myBucket = new AWS.S3({
+        params: { Bucket: S3_BUCKET},
+        region: REGION,
+    })
 
 
     function refreshPage() {
         window.location.reload(false);
     }
+    
+    const handleFileInput = (e) => {
+
+        selectedFile = e.target.files[0];
+    }
+
+    const handleUpload = async () => {
+        const params = {
+            ACL: 'public-read',
+            Body: selectedFile,
+            Bucket: S3_BUCKET,
+            Key: selectedFile.name
+        };
+
+        myBucket.putObject(params)
+            .on('httpUploadProgress', (evt) => {
+                setProgress(Math.round((evt.loaded / evt.total) * 100))
+            })
+            .send((err) => {
+                if (err) console.log(err)
+            })
+    }
 
     const onSubmit=(e)=>{
         e.preventDefault()
+        handleUpload()
+        imageLink = "https://cloudcomputing-a1-images.s3.amazonaws.com/" + selectedFile.name
         const item={itemName, desc, category, pricePD, colour, size, sex, available, imageLink}
         console.log(item)
         fetch(`http://cloudcomputinga1itemmicroservice-env.eba-btrpvvm3.us-east-1.elasticbeanstalk.com/api/items/addItem`, {
@@ -94,7 +148,6 @@ export default function AddItem() {
                                     <option value="Extra Small">Extra Small</option>
                                     <option value="Small">Small</option>
                                     <option value="Medium">Medium</option>
-                                    <option value="Extra Medium">EXTRA MEDIUM</option>
                                     <option value="Large">Large</option>
                                     <option value="Extra Large">Extra Large</option>
                                 </select>
@@ -117,6 +170,11 @@ export default function AddItem() {
                                     <option value="Uni-Sex">Uni-Sex</option>
                                 </select>
                             </div><br></br>
+                            <div className="form-group">
+                                <p className="display-5">Upload an Image</p><br></br>
+                                <input type="file" onChange = {handleFileInput} 
+                                required/>
+                            </div>
                     <input type="submit" className="btn btn-primary btn-block mt-4"/>
                     </form>
                     </div>
